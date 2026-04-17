a78a466d3965288606dd03c4873f1181cfe5e71e
bangzay54 <44945212+bangzay54@users.noreply.github.com>
2026-04-17 15:55:40 +0700

    Add files via upload

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,35 +1,35 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
-  author: "Alejandro Múnez Cuntez",
+  title: "Ita Sophiatu Rojak — Voice Over and Proofreader",
+  author: "Ita Sophiatu Rojak",
   description:
     "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
   lang: "en",
-  siteLogo: "/alejandro-small.jpg",
+  siteLogo: "/ita-small.jpg",
   navLinks: [
     { text: "Experience", href: "#experience" },
     { text: "Projects", href: "#projects" },
     { text: "About", href: "#about" },
   ],
   socialLinks: [
-    { text: "Twitter", href: "https://github.com/immois/astro-zen" },
-    { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
-    { text: "Github", href: "https://github.com/immois/astro-zen" },
-    { text: "Youtube", href: "https://github.com/immois/astro-zen" },
-    { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
+    // { text: "Twitter", href: "https://github.com/immois/astro-zen" },
+    // { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
+    // { text: "Github", href: "https://github.com/immois/astro-zen" },
+    // { text: "Youtube", href: "https://github.com/immois/astro-zen" },
+    // { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
   ],
   socialImage: "/zen-og.png",
-  canonicalURL: "https://astro-zen.vercel.app",
+  canonicalURL: "https://itasophiatuporto.vercel.app/",
 };
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
-    specialty: "Mobile & Web Developer",
+    name: "Ita Sophiatu Rojak",
+    specialty: "Voice Over & Proofreader",
     summary:
       "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-    email: "example@email.com",
+    email: "ita.s.rajak@gmail.com",
   },
   experience: [
     {
@@ -91,7 +91,7 @@ export const SITE_CONTENT: SiteContent = {
 
       Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
     `,
-    image: "/alejandro-big.jpg",
+    image: "/ita-big.jpg",
   },
 };
 
